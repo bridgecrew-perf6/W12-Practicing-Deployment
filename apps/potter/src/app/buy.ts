279b21459405f7b4d164bd7ccb6084904ed63684
cy <yinoao@gmail.com>
2022-05-08 15:42:03 +0800

    第二個綠燈

--- apps/potter/src/app/buy.ts
+++ apps/potter/src/app/buy.ts
@@ -1,9 +1,17 @@
 export class Buy {
-
-  choose(episode: number) {}
+  private _total = 0;
+  //private count: number[] = [0, 0, 0, 0, 0, 0, 0, 0]
+  choose(episode: number) {
+    if (this._total === 0) {
+      this._total += 100;
+    }else{
+      this._total += 100;
+      this._total *= 0.95;
+    }
+  }
 
   get total() {
-    return 100;
+    return this._total;
   }
 
 }
