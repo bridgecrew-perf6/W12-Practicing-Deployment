58e4b2d2f728d1f3c1fe714baf53f8ac84670fb6
cy <yinoao@gmail.com>
2022-05-08 16:03:55 +0800

    第三個綠燈

--- apps/potter/src/app/buy.ts
+++ apps/potter/src/app/buy.ts
@@ -1,16 +1,22 @@
 export class Buy {
   private _total = 0;
+  private numofep = 0;
+  //private numofbook = 0;
   //private count: number[] = [0, 0, 0, 0, 0, 0, 0, 0]
   choose(episode: number) {
-    if (this._total === 0) {
-      this._total += 100;
-    }else{
-      this._total += 100;
-      this._total *= 0.95;
-    }
+      this.numofep ++;
   }
 
   get total() {
+    if (this.numofep === 1){
+      this._total += 100;
+    }else if (this.numofep === 2){
+      this._total += 200;
+      this._total *= 0.95;
+    }else if (this.numofep === 3){
+      this._total += 300;
+      this._total *= 0.9;
+    }
     return this._total;
   }
 
